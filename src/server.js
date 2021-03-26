faafe191ffe224bd654f3672d94ea682bebe307a
Nadia <nlnadialigia@gmail.com>
2021-03-26 14:20:03 -0300

    feat: show index.html by server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import path from 'path';
 
 const server = express()
+const __dirname = path.resolve(path.dirname('')); 
+
+server.use(express.static('public'))
+
+server.get('/', (request, response) => {
+  return response.sendFile(`${__dirname}/views/index.html`)
+})
 
 server.listen(5010, () => console.log('Server is running!'))
