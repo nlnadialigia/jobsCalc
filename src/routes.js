0c995a192e6457096b93cffe1b734098d835a706
Nadia <nlnadialigia@gmail.com>
2021-03-26 15:37:45 -0300

    refactor: fix file Path and href

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,14 @@
 import { Router } from 'express';
 import path from 'path';
 
-const __dirname = path.resolve(path.dirname(''));
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  response.sendFile(`${__dirname}/views/index.html`);
-});
+const __dirname = path.resolve(path.dirname(''));
+const basePath = `${__dirname}/views`;
+
+routes.get('/', (request, response) => response.sendFile(`${basePath}/index.html`));
+routes.get('/job', (request, response) => response.sendFile(`${basePath}/job.html`));
+routes.get('/job/edit', (request, response) => response.sendFile(`${basePath}/job-edit.html`));
+routes.get('/profile', (request, response) => response.sendFile(`${basePath}/profile.html`));
 
 export default routes;
